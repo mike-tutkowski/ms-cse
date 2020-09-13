18c4367f466feb67a4adb34e992e2f51f9c7c9a5
Tut-MacBook <tutkowski@Tut-MacBooks-MacBook-Pro.local>
2020-09-13 16:11:44 -0600

    Make the getLocations and getTaxiQuery methods synchronized (a comment in the code explains why).

--- src/main/java/service/ServiceImpl.java
+++ src/main/java/service/ServiceImpl.java
@@ -14,6 +14,14 @@ import org.apache.log4j.Logger;
 import schema.LocationTable;
 import schema.TransportType;
 
+/** For the purposes of this assignment, I decided to go with an embedded Derby DB. This makes configuration easy
+ *  (so I can spend more time on other aspects of the project), but it only allows for one active connection at a
+ *  time. As such, I have made the getLocations and getTaxiQuery methods synchronized so that only one of them
+ *  is accessing the DB at a time. In a real-world situation, we would want to spend time making sure the DB
+ *  can not only be accessed from multiple threads at the same time, but that our code can control access to
+ *  such resources properly.
+ */
+
 /**
  * The ServiceImpl class, which implements the Service interface, represents
  * the business logic of the system.
@@ -24,7 +32,7 @@ public class ServiceImpl implements Service {
     private static final String CONNECTION_URL =
             "jdbc:derby:/Users/tutkowski/Downloads/db-derby-10.14.2.0-bin/lib/nytripdb";
 
-    public List<Location> getLocations(String startsWith) throws Exception {
+    public synchronized List<Location> getLocations(String startsWith) throws Exception {
         String columnNameId = LocationTable.ID.toString();
         String columnNameBorough = LocationTable.BOROUGH.toString();
         String columnNameZone = LocationTable.ZONE.toString();
@@ -61,7 +69,7 @@ public class ServiceImpl implements Service {
         return boroughs;
     }
 
-    public TaxiQuery getTaxiQuery(int fromLocationId, int toLocationId, TransportType type) throws Exception {
+    public synchronized TaxiQuery getTaxiQuery(int fromLocationId, int toLocationId, TransportType type) throws Exception {
         /** @// TODO: 9/13/20 Replace with access to the DB */
         return new TaxiQuery(500, 10.50f);
     }
